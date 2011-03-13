ef3ae512a0b8907643ba51a8e436e369bc88c889
Maik Hentsche <maik@mm-double.de>
2011-03-13 12:52:20 +0100

    Any/Any als cooles Junction-Beispiel

--- Beispiel/Perl6/junction.pl
+++ Beispiel/Perl6/junction.pl
@@ -6,5 +6,7 @@ ok( all(10,11,12,13) >= 10,              'All funktioniert');
 ok( none('first', 'second') eq 'third',  'None funktioniert');
 ok( one(10,11,12,13) == 10,              'One funktioniert');
 
+ok(any(1,2,3) == any (4,3,5), 'Any mit Any');
+
 
 done_testing();
